dcf31f39279633261958ab193c972b7c8ebcd2ff
Gabriel S Santiago <gabriel.santiago16@upr.edu>
2023-09-08 23:17:09 -0400

    added fdfind ignore

--- install.sh
+++ install.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-STOW_FOLDERS="vim,tmux,bin,nvm,yarn,bash,fzf,git"
+STOW_FOLDERS="vim,tmux,bin,nvm,yarn,bash,fzf,git,fdfind"
+STOW_DIR="$HOME"
+
+if [[ -e ~/.bashrc.bk ]] || [[ -e ~/.profile.bk ]]; then
+    printf "%s\n" ".dotfiles: init error: Already have .bk files. Make sure you are not overriding previous backups." >&2;
+    exit 1;
+fi
+
+mv ~/.bashrc ~/.bashrc.bk
+mv ~/.profile ~/.profile.bk
+
+pushd ~/.dotfiles
 
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
 do
-    echo "stow $folder"
-    stow -S $folder -t $HOME
+    echo ".dotfiles: $folder stowed to $STOW_DIR"
+    stow -S $folder -t $STOW_DIR
 done
+
+popd
+
+echo ".dotfiles: success: Run \"source ~/.bashrc\" to load changes"
